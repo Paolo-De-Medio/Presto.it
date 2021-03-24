e8d48ac6c51f6d51da20ba1ac44c58523006cb96
Davide Cariola <davidecariola90@gmail.com>
2021-03-24 20:09:35 +0100

    Autofill Jv per ads. Check colonne. Check navbar schermi piccoli. Carousel in sospeso.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let categoryWrapper = document.querySelector('#catWrapper');
+
 function pushCategories () {
     let categories = [
         {cat: 'Veicoli' , icon: '<i class="fas fa-car display-5 tc-main"></i>'},
@@ -9,10 +11,9 @@ function pushCategories () {
     ]
     console.log(categories)
 
-    let categoryWrapper = document.querySelector('#catWrapper');
+    if(categoryWrapper){
 
     categories.forEach(category => {
-        console.log(category)
         let card = document.createElement ('div');
         card.classList.add('col-12' , 'col-md-4');
 
@@ -29,6 +30,7 @@ function pushCategories () {
 
     })
 }
+}
 
 pushCategories()
 
@@ -37,3 +39,34 @@ btnMenu.addEventListener('click' , () => {
     let menuIcon = document.querySelector('#menu-icon')
     menuIcon.classList.toggle('nav-effect')
 })
+
+// let adsWrapper = document.querySelector('#adsWrapper');
+// if(adsWrapper) {
+// function pushAds () {
+    
+//     annunci.forEach(annuncio => {
+//     let adsCard = document.createElement ('div');
+
+//     adsCard.innerHTML = `
+//             <div class="col-6">
+//                 <div class="card p-3 mt-4" style="max-width: 540px;">
+//                     <div class="row g-0">
+//                       <div class="col-md-4">
+//                         <img src="https://picsum.photos/200" alt="...">
+//                       </div>
+//                       <div class="col-md-8">
+//                         <div class="card-body">
+//                           <h5 class="card-title fw-bold ps-3">${annuncio.name}</h5>
+//                           <p class="card-text ps-3">${annuncio.price}</p>
+//                           <p class="card-text ps-3"><small class="text-muted">${annuncio.category}</small></p>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//             </div>
+//     `
+//     adsWrapper.appendChild(adsCard)
+// })
+// }
+// }
+pushAds()
