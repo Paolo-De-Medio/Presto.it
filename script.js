676ac7e5fc67b8abebfc3807260b7ed0d4934969
Davide Cariola <davidecariola90@gmail.com>
2021-03-25 15:05:02 +0100

    Creazione navbar laterla c/filtri; risolto schede.

--- script.js
+++ script.js
@@ -98,3 +98,14 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+$('#multi').mdbRange({
+    single: {
+      active: true,
+      multi: {
+        active: true,
+        rangeLength: 1
+      },
+    }
+  });
