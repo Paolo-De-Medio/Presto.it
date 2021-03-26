eb9414e48aa0f7c767d57818bc32dcc048f03f4e
Davide Cariola <davidecariola90@gmail.com>
2021-03-26 23:13:00 +0100

    Miglioramenti grafici index

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ let ultimiAnnunci = document.querySelector('.lastAds')
                     <div class=" vertical-center card bg-dark text-white mx-4">
                         <img class="card-img img-fluid" src="https://picsum.photos/600" alt="Card image">
                         <div class="col-12 card-img-overlay p-4 ad-card-overlay text-center">
-                            <h5 class="card-title tc-accent fw-bold fs-4">${annuncio.titolo}</h5>
+                            <h5 class=" mt-5 card-title tc-accent fw-bold fs-4">${annuncio.titolo}</h5>
                             <p class="card-text">${annuncio.descrizione}</p>
                             <p class="card-text">${annuncio.prezzo}</p>
-                            <button class="btn btn-outline-main">Vai all'annuncio</button>
+                            <button class="mt-2git  text-center mb-2 btn btn-color ">Vai all'annuncio</button>
                             <div class="row justify-content-between ">
                                 <div class="col-12 position-absolute start-0 btn-bottom-1">
                                     
