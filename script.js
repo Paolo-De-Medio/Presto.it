7e3754086084fa0372c3927f59761b7550a5c3b9
Paolo De Medio <paolodemedio72@outlook.com>
2021-05-16 20:17:46 +0200

    mobile-friendly migliorato

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function pushCategories () {
         card.classList.add('col-12' , 'col-md-4');
 
         card.innerHTML = `
-            <div class="card-custom text-center pt-3 my-4">
+            <div class="card-custom text-center pt-3 my-4 mx-auto">
             ${category.icon}
                 <div class="card-body">
                     <h5 class="card-title tc-main">${category.cat}</h5>
@@ -58,20 +58,20 @@ let ultimiAnnunci = document.querySelector('.lastAds')
 
                     card.innerHTML=
                     `
-                    <div class=" vertical-center card bg-dark text-white mx-2">
+                    <div class="vertical-center card bg-dark text-white mx-2">
                         <img class="card-img img-fluid" src="https://picsum.photos/600" alt="Card image">
                         <div class="col-12 card-img-overlay ad-card-overlay text-center">
                             <h5 class=" mt-5 card-title tc-accent fw-bold fs-4">${annuncio.titolo}</h5>
                             <p class="card-text">${annuncio.descrizione}</p>
                             <p class="card-text">${annuncio.prezzo}</p>
                             <button class="mt-2 text-center mb-2 btn btn-color ">Vai all'annuncio</button>
-                            <div class="row justify-content-between ">
+                            <div class="row justify-content-between">
                             <i class="far fa-heart mt-3 fs-3 tc-accent heart-like"></i>
                                 <div class="col-12 position-absolute start-0">
                                 
                                     
                                 </div>
-                                <div class="col-3 position-absolute end-0 btn-bottom-1"></div>
+                                <div class="col-2 position-absolute end-0 btn-bottom-1"></div>
                             </div>
                         </div>
                     </div>
